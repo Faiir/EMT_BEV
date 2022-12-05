681df5753f764e7b3190853124f9390e47bdbc58
niklas <nk@niklaskrauss.com>
2022-12-05 14:39:30 +0100

    update install sbatch

--- sbatch_beverse.sh
+++ sbatch_beverse.sh
@@ -14,5 +14,7 @@ source /home/kraussn/anaconda3/bin/activate /home/kraussn/anaconda3/envs/motion_
 cd /home/kraussn/EMT_BEV/  # navigate to the directory if necessary
 
 
+srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 setup.py develop
+
 srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 /home/kraussn/EMT_BEV/tools/create_data.py nuscenes --root-path /home/kraussn/EMT_BEV/data/nuscenes --out-dir /home/kraussn/EMT_BEV/data/nuscenes --extra-tag nuscenes
 # srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 train_debugg_cluster.py 
