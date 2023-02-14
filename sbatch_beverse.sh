c4ed91d6f0b46e653243a64ebf3c8fab051dde1f
niklas <nk@niklaskrauss.com>
2023-02-14 11:54:22 +0100

    sbatch update

--- sbatch_beverse.sh
+++ sbatch_beverse.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=beverse_train
-#SBATCH --output=beverse_train%j.log
-#SBATCH --error=beverse_train%j.err
+#SBATCH --job-name=beverse_motion_benchmark_test
+#SBATCH --output=beverse_motion_benchmark%j.log
+#SBATCH --error=beverse_motion_benchmark%j.err
 #SBATCH --mail-user=kraussn@uni-hildesheim.de
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
@@ -14,8 +14,4 @@ source /home/kraussn/anaconda3/bin/activate /home/kraussn/anaconda3/envs/motion_
 cd /home/kraussn/EMT_BEV/  # navigate to the directory if necessary
 
 
-srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 setup.py develop
-srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 home/kraussn/EMT_BEV/projects/mmdet3d_plugin/models/ops/setup.py build install
-srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 /home/kraussn/EMT_BEV/tools/create_data.py nuscenes --root-path /home/kraussn/EMT_BEV/data/nuscenes --out-dir /home/kraussn/EMT_BEV/data/nuscenes --extra-tag nuscenes
-# srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 train_debugg_cluster.py 
-# home/kraussn/EMT_BEV/projects/mmdet3d_plugin/models/ops/setup.py build install
+srun /home/kraussn/anaconda3/envs/motion_detr/bin/python3 /home/kraussn/EMT_BEV/full_benchmark.py
